f30a29c42179dc22a734493c71ffff85c0c3b6f2
Pandaaa-C <pandacodeyt@gmail.com>
2024-10-18 14:44:46 +0100

    update

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    document.getElementById("hacking-image").onclick = function() {
+        window.open("https://app.hackthebox.com/", "_blank");
+    };
+    
+    document.getElementById("coding-image").onclick = function() {
+        window.open("https://w3schools.com/", "_blank");
+    };
+};
